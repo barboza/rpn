fd4fa07588845d9a63ebe09b20924198a6ff280f
Rafael Barboza <barboza.cardoso@gmail.com>
2013-01-18 13:36:38 -0200

    added near places to the home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if request.location.city == ""
+      @places = Place.all
+    else
+      @places = Place.near(request.location.city, 50, :order => :distance)
+    end
   end
 end
